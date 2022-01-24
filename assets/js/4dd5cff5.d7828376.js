d7f76690e4ee7efa59c8ca628e64d05fbae783e8
Leo Yu Lee <leoyulee.585@gmail.com>
2022-01-24 15:51:06 -0600

    Built and published by Moonwave

--- assets/js/4dd5cff5.d7828376.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[698],{2824:function(s){s.exports=[]}}]);
---
+++ assets/js/9b31b3bb.8297c8ef.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[690],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/leoyulee/Roblox-Ballistics-Functions/blob/main","baseUrl":"/Roblox-Ballistics-Functions/","classOrder":[],"apiCategories":[]}')}}]);
